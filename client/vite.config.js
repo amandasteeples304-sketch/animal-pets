80137311168c61f17c83cfe0273b95f86b359e59
Amanda Steeples <amandasteeples304@gmail.com>
2026-02-04 10:04:01 +0000

    fix client building comment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,13 @@
-
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                petPage: resolve(__dirname, "pages/petPage/index.html"),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        petpage: resolve(__dirname, "pages/petPage/index.html"),
+      },
     },
+  },
 });
