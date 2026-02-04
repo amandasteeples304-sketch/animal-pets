432b3f166fdef42b580ee10ec8d44c8a5532c918
Amanda Steeples <amandasteeples304@gmail.com>
2026-02-04 13:53:59 +0000

    stats exist on the page but there is still a nan on the mps

--- client/pages/petPage/PETpetPage.js
+++ client/pages/petPage/PETpetPage.js
@@ -19,7 +19,6 @@ let saveGame = document.getElementById("saveGame");
 function game() {
   loadGame();
   setInterval(function () {
-    saveGame();
     state.mps = state.mps + state.stats;
     mpsDisplay.innerText = state.mps;
     statsDisplay.innerText = state.stats;
@@ -27,33 +26,10 @@ function game() {
 }
 
 image.addEventListener("click", function () {
-  state.mps += 1;
+  state.mps++;
   mpsDisplay.innerText = state.mps;
   console.log(state.mps);
 });
-
-// function loadGame() {
-//   console.log(localStorage.getItem("state"));
-//   if (localStorage.getItem("state") === null) {
-//     return;
-//   }
-//   state = JSON.parse(localStorage.getItem("state"));
-// }
-// game();
-
-// function resetGame() {
-//   localStorage.clear();
-//   state = {
-//     stats: 0,
-//     mps: 1,
-//     items: [],
-//   };
-//   const resetButton = document.createElement("button");
-//   resetButton.classList.add("reset-button");
-//   resetButton.innerText = "Reset Game";
-
-//   resetButton.addEventListener("click", resetGame);
-// }
 
 const item = [
   { itemName: "Bookstore", itemCost: 10, itemIncrease: 2 },
@@ -66,12 +42,16 @@ function generateShop() {
   item.forEach(function (item) {
     const itemContainer = document.createElement("div");
     itemContainer.classList.add("shop-item");
+
     const itemName = document.createElement("p");
     itemName.innerText = item.itemName;
+
     const itemCost = document.createElement("p");
     itemCost.innerText = "Cost: " + item.itemCost;
+
     const itemIncrease = document.createElement("p");
     itemIncrease.innerText = "Increase: " + item.itemIncrease;
+
     const buyButton = document.createElement("button");
     buyButton.classList.add("buy-button");
     buyButton.innerText = "Buy";
@@ -79,28 +59,49 @@ function generateShop() {
     buyButton.addEventListener("click", function () {
       purchaseItem(item);
     });
+
     itemContainer.append(itemName, itemCost, itemIncrease, buyButton);
+
     shop.append(itemContainer);
   });
+
   console.log(item);
 
-  const buyButton = document.createElement("button");
-  buyButton.classList.add("buy-button");
-  buyButton.innerText = "Buy";
+  // const buyButton = document.createElement("button");
+  // buyButton.classList.add("buy-button");
+  // buyButton.innerText = "Buy";
 
-  buyButton.addEventListener("click", function () {
-    purchaseItem(items);
-  });
+  // buyButton.addEventListener("click", function () {
+  //   purchaseItem(items);
+  // });
 }
 
 generateShop();
 
 let stats = [
-  { statsKnowledge: "Knowledge", value: 0 },
-  { statsHealth: "Health", value: 0 },
-  { statsNutrition: "Nutrition", value: 0 },
-  { statsHappiness: "Happiness", value: 0 },
+  { statsName: "Knowledge", statsValue: 0 },
+  { statsName: "Health", statsValue: 0 },
+  { statsName: "Nutrition", statsValue: 0 },
+  { statsName: "Happiness", statsValue: 0 },
 ];
+
+function generateStats() {
+  stats.forEach(function (stats) {
+    const statsContainer = document.createElement("div");
+    statsContainer.classList.add("stats-item");
+
+    const statsName = document.createElement("p");
+    statsName.innerText = stats.statsName;
+
+    const statsValue = document.createElement("p");
+    statsValue.innerText = stats.statsValue;
+
+    statsContainer.append(statsName, statsValue);
+    statsDisplay.append(statsContainer);
+  });
+}
+
+generateStats();
 
 function purchaseItem(itemParam) {
   if (itemParam.cost > state.mps) {
@@ -109,5 +110,6 @@ function purchaseItem(itemParam) {
   }
 
   state.mps -= itemParam.cost;
+  state.mps = +itemParam.increase;
   state.stats += itemParam.increase;
 }
